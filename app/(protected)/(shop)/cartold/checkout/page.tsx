725b97614e379e6d7f23e716afa6f9524ff4a701
EvgenyTj <38112505+TjLinka@users.noreply.github.com>
2026-07-31 15:26:08 +0300

    1

--- app/(protected)/(shop)/cartold/checkout/page.tsx
+++ app/(protected)/(shop)/cartold/checkout/page.tsx
@@ -20,7 +20,10 @@ import {
 import { Dialog } from "primereact/dialog";
 import { InputText } from "primereact/inputtext";
 import { useCallback, useEffect, useState } from "react";
+<<<<<<<< HEAD:app/(protected)/(shop)/cartold/checkout/page.tsx
 import { HeaderProductCard } from "../../catalogold/components/HeaderProductCard";
+========
+>>>>>>>> 69141b3 (1):app/cart/checkout/page.tsx
 import { localInt } from "@/lib/utils";
 import { InputNumber } from "primereact/inputnumber";
 import { Nullable } from "primereact/ts-helpers";
@@ -33,6 +36,7 @@ import {
 import _ from "lodash";
 import PickupMap from "@/app/(protected)/test/components/PickupMap";
 import { RadioButton } from "primereact/radiobutton";
+import { HeaderProductCard } from "@/app/catalog/components/HeaderProductCard";
 
 export default function CartCheckoutPage() {
   const { data } = useQuery({
